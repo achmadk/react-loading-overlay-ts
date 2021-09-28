9e73ed26a7457e3196dd71be9e30fd657376201d
Achmad Kurnianto <achmad.kurnianto@gmail.com>
2021-09-28 10:29:48 +0700

    fix(test): :white_check_mark: update test suite
    add annotation to set test environment to `jsdom` instead of `node`

--- test/LoadingOverlay.test.tsx
+++ test/LoadingOverlay.test.tsx
@@ -1,3 +1,7 @@
+/**
+ * @jest-environment jsdom
+ */
+
 import React, { Component } from 'react';
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
