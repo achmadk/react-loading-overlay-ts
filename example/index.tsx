52f7e1736693c1ff422cd43e21691446125d4d45
Achmad Kurnianto <achmad.kurnianto@gmail.com>
2023-02-20 12:04:29 +0800

    refactor(example): :recycle: use `createRoot` instead `render` from `react-dom`
    use new method from `react-dom/client` called `createRoot` instead of `render`.

--- example/index.tsx
+++ example/index.tsx
@@ -1,33 +1,42 @@
 import 'react-app-polyfill/ie11';
 import * as React from 'react';
-import { render } from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import LoadingOverLay, { LoadingOverLayProps } from '../.';
 
 const App = () => {
+  const [active, setActive] = React.useState(true)
   const overlayRef = React.useRef<HTMLDivElement>(null as unknown as HTMLDivElement)
+
+  const handleToggleActiveButton = React.useCallback(() =>
+    setActive(prevVal => !prevVal)
+  , [])
 
   return (
     <React.StrictMode>
-      <LoadingOverLay
-        ref={overlayRef}
-        active={true}
-        spinner={true}
-        text="Loading..."
-        styles={{
-          wrapper: (base: any, props: LoadingOverLayProps) => {
-            console.log(base, props)
-            return {
-              ...base,
-              backgroundColor: 'red'
+      <>
+        <LoadingOverLay
+          ref={overlayRef}
+          active={active}
+          spinner={true}
+          text="Loading..."
+          styles={{
+            wrapper: (base: any, props: LoadingOverLayProps) => {
+              console.log(base, props)
+              return {
+                ...base,
+                backgroundColor: 'red'
+              }
             }
-          }
-        }}>
-        <div style={{ height: 100 }}>
-          hello
-        </div>
-      </LoadingOverLay>
+          }}>
+          <div style={{ height: 100 }}>
+            hello
+          </div>
+        </LoadingOverLay>
+        <button onClick={handleToggleActiveButton}>Show/hide overlay</button>
+      </>
     </React.StrictMode>
   );
 };
 
-render(<App />, document.getElementById('root'));
+const root = createRoot(document.getElementById('root') as HTMLElement);
+root.render(<App />);
